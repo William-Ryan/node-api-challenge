3f061fc48ba664f95ca9e7338ae1e87d5c9f0cf1
William <billy.ryan.rom@gmail.com>
2020-03-13 12:05:43 -0400

    Add get request for actions to actionRouter.js

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -3,4 +3,15 @@ const express = require('express');
 const Actions = require("../data/helpers/actionModel.js")
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    Actions.get()
+    .then(action => res.json(action))
+    .catch(err => {
+        console.log("Error: ", err);
+        res.status(500).json({
+            message: "The Actions could not be displayed"
+        })
+    })
+})
+
 module.exports = router
