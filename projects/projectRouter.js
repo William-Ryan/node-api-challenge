f7b428726a4869df2ee80d870071f2995f5ed76d
William <billy.ryan.rom@gmail.com>
2020-03-13 11:34:42 -0400

    Add GET Request to projectRouter.js and import into server.js

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -3,4 +3,15 @@ const express = require('express');
 const Projects = require("../data/helpers/projectModel")
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    Projects.get()
+    .then(project => res.json(project))
+    .catch(err => {
+        console.log("Error: ", err);
+        res.status(500).json({
+            message: "The Projects could not be displayed"
+        })
+    })
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const helmet = require('helmet')
+
+const projectRouter = require("./projects/projectRouter.js")
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use("/api/projects", projectRouter)
 
 server.get('/', logger, (req, res) => {
     res.send(`<h1>Let's Do This!!!</h1>`)
