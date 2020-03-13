f7df4b4a0812aad81f61aefebb8bb6e1a557e11e
William <billy.ryan.rom@gmail.com>
2020-03-13 12:19:03 -0400

    Add Post, Delete, and Put Request for actions to actionRouter.js

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -14,4 +14,43 @@ router.get("/", (req, res) => {
     })
 })
 
+router.post("/", (req, res) => {
+    Actions.insert(req.body)
+    .then(action => {
+        res.status(201).json(action)
+    })
+    .catch(err => {
+        console.log("Error: ", err);
+        res.status(500).json({ message: "Error adding the action" })
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    Actions.remove(req.params.id)
+    .then(() => {
+        res.status(200).json({ message: "Action has been removed" })
+    })
+    .catch(err => {
+        console.log("Error: ", err);
+        res.status(500).json({ 
+            message: "The Action could not be removed"
+        })
+    })
+})
+
+router.put("/:id", (req, res) => {
+    Actions.update(req.params.id, req.body)
+    .then(() => {
+        res.status(200).json({
+            message: "The Action has been updated"
+        })
+    })
+    .catch(err => {
+        console.log("Error: ", err)
+        res.status(500).json({ 
+            message: "The Action could not be updated"
+        })
+    })
+})
+
 module.exports = router
