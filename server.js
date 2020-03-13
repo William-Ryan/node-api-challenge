f9152494ec8544eb67d69990b56b1e65be0097cb
William <billy.ryan.rom@gmail.com>
2020-03-13 12:02:03 -0400

    Add actionRouter.js import in actionModel.js/Import actionRouter.js to server.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const helmet = require('helmet')
 
 const projectRouter = require("./projects/projectRouter.js")
+const actionRouter = require("./actions/actionRouter")
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use("/api/projects", projectRouter)
+server.use("/api/actions", actionRouter)
 
 server.get('/', logger, (req, res) => {
     res.send(`<h1>Let's Do This!!!</h1>`)
