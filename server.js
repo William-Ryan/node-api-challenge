e015d38335d20a17fd88efebeb13b23ed9eaf593
William <billy.ryan.rom@gmail.com>
2020-03-13 11:27:21 -0400

    Add logger middleware and helmet to server.js/Add projectRouter.js

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
     res.send(`<h1>Let's Do This!!!</h1>`)
 });
+
+function logger(req, res, next) {
+    const method = req.method;
+    const endpoint = req.originalUrl;
+  
+    console.log(`${method} to ${endpoint}`);
+  
+    next();
+}
 
 module.exports = server;
